30c3768cdd13f6471a0be2ebc02d590fb524fd64
Ashish Joshi <85393254+Delimiter-Ashish@users.noreply.github.com>
2026-01-15 20:28:14 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -19,4 +19,10 @@ app = create_app()
 app.register_blueprint(dashboard_blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=False,
+        use_reloader=False
+    )
+
